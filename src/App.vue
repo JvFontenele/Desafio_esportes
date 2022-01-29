1baee2a6c23667e2023b71ecd6e0a9e05baeda03
JvFontenele <jvfont80@gmail.com>
2022-01-29 19:07:44 -0300

    fiest commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,57 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-app-bar app color="red lighten-1" flat>
+      <v-container class="py-0 fill-height">
+        <v-img
+          link
+          src="./assets/LogoPowerLeagues.png"
+          max-height="50"
+          max-width="50"
+          class="ml-12 mr-12"
+        ></v-img>
+
+        <v-btn v-for="link in links" :key="link" text class="mr-2">
+          <div class="white--text">{{ link }}</div>
+        </v-btn>
+
+        <v-spacer></v-spacer>
+
+        <v-responsive max-width="260">
+          <v-text-field
+            dense
+            flat
+            hide-details
+            rounded
+            solo-inverted
+          ></v-text-field>
+        </v-responsive>
+      </v-container>
+    </v-app-bar>
+
+    <v-main class="grey lighten-2">
+      <v-container>
+        <v-row>
+          <v-col cols="2"> <Leagues /> </v-col>
+          <v-col>
+            <v-sheet min-height="70vh" rounded="lg">
+              <router-view />
+              <!--  -->
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+import Leagues from "@/components/Leagues.vue";
+export default {
+  data: () => ({
+    links: ["Times", "Listas"],
+  }),
+  components: {
+    Leagues,
+  },
+};
+</script>
---
+++ src/components/Leagues.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-sheet rounded="lg">
+    <v-list
+      color="transparent"
+      max-height="88vh"
+      class="scrolStyle pa-3 overflow-auto"
+    >
+      <v-list-item v-for="league in leagues.data" :key="league.id">
+        <v-list-item-content class="mx-auto d-flex justify-center">
+          <v-card max-height="100" max-width="100" class="pa-1" link>
+            <v-img
+              max-height="80"
+              max-width="80"
+              :src="league.logos.light"
+              class="mx-auto"
+            ></v-img
+          ></v-card>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-sheet>
+</template>
+
+<script>
+import { api } from "../services.js";
+
+export default {
+  name: "leagues",
+  data() {
+    return {
+      leagues: [],
+    };
+  },
+  methods: {
+    getApi() {
+      api
+        .get("https://api-football-standings.azharimm.site/leagues")
+        .then((response) => {
+          this.leagues = response.data;
+        });
+    },
+  },
+  created() {
+    this.getApi();
+  },
+};
+</script>
+<style>
+.scrolStyle::-webkit-scrollbar {
+  width: 2px;
+  height: 2px;
+}
+</style>
